6aa945d5fcfe273e8c87f469bc0e6292b5f580af
nickspoon <nick@jense.nz>
2019-12-15 11:12:39 +1300

    Day 12 b

--- cs/Advent2019/Day12.cs
+++ cs/Advent2019/Day12.cs
@@ -31,19 +31,62 @@ namespace AdventOfCode.Advent2019 {
                   .Select(combos => combos.Prepend(element).ToArray()));
       }
 
+      private long GCD(long a, long b) {
+         a = Math.Abs(a);
+         b = Math.Abs(b);
+         while (b > 0) {
+            long remainder = a % b;
+            a = b;
+            b = remainder;
+         }
+         return a;
+      }
+
+      private (int, int, int, int) GetPos(int c) {
+         return (
+            Moons[0].Pos[c],
+            Moons[1].Pos[c],
+            Moons[2].Pos[c],
+            Moons[3].Pos[c]
+         );
+      }
+
+      private (int, int, int, int) GetVel(int c) {
+         return (
+            Moons[0].Vel[c],
+            Moons[1].Vel[c],
+            Moons[2].Vel[c],
+            Moons[3].Vel[c]
+         );
+      }
+
+      private long LCD(long a, long b) {
+         return a * b / GCD(a, b);
+      }
+
+      private long LCD(long a, long b, long c) {
+         return LCD(a, LCD(b, c));
+      }
+
       private void UpdatePosition(Moon moon) {
-         moon.X += moon.VX;
-         moon.Y += moon.VY;
-         moon.Z += moon.VZ;
+         for (int c = 0; c < 3; c++)
+            moon.Pos[c] += moon.Vel[c];
+      }
+
+      private void UpdatePosition(Moon moon, int c) {
+         moon.Pos[c] += moon.Vel[c];
       }
 
       private void UpdateVelocities(Moon[] moons) {
-         moons[0].VX += Compare(moons[0].X, moons[1].X);
-         moons[1].VX += Compare(moons[1].X, moons[0].X);
-         moons[0].VY += Compare(moons[0].Y, moons[1].Y);
-         moons[1].VY += Compare(moons[1].Y, moons[0].Y);
-         moons[0].VZ += Compare(moons[0].Z, moons[1].Z);
-         moons[1].VZ += Compare(moons[1].Z, moons[0].Z);
+         for (int c = 0; c < 3; c++) {
+            moons[0].Vel[c] += Compare(moons[0].Pos[c], moons[1].Pos[c]);
+            moons[1].Vel[c] += Compare(moons[1].Pos[c], moons[0].Pos[c]);
+         }
+      }
+
+      private void UpdateVelocities(Moon[] moons, int c) {
+         moons[0].Vel[c] += Compare(moons[0].Pos[c], moons[1].Pos[c]);
+         moons[1].Vel[c] += Compare(moons[1].Pos[c], moons[0].Pos[c]);
       }
 
       public override string A() {
@@ -58,27 +101,50 @@ namespace AdventOfCode.Advent2019 {
       }
 
       public override string B() {
-         return "";
+         int count = 0;
+         Moon[][] pairs = Combinations(Moons, 2).ToArray();
+         (int, int, int, int)[] origins = Enumerable.Range(0, 3)
+            .Select(c => GetPos(c))
+            .ToArray();
+         int[] periods = new[] { -1, -1, -1 };
+         int done = 0;
+         while (done < 3) {
+            done = 0;
+            count++;
+            for (int c = 0; c < 3; c++) {
+               if (periods[c] > 0) {
+                  done++;
+                  continue;
+               }
+               foreach (Moon[] pair in pairs)
+                  UpdateVelocities(pair, c);
+               foreach (Moon moon in Moons)
+                  UpdatePosition(moon, c);
+               if (GetPos(c) == origins[c] && GetVel(c) == (0, 0, 0, 0)) {
+                  periods[c] = count;
+                  done++;
+               }
+            }
+            if (count % 1000000 == 0)
+               Console.WriteLine(count);
+         }
+
+         return LCD(periods[0], periods[1], periods[2]).ToString();
       }
 
       private class Moon {
          public Moon() { }
          public Moon(int x, int y, int z) {
-            X = x;
-            Y = y;
-            Z = z;
+            Pos[0] = x;
+            Pos[1] = y;
+            Pos[2] = z;
          }
 
-         public int X { get; set; }
-         public int Y { get; set; }
-         public int Z { get; set; }
+         public int[] Pos { get; } = new int[3];
+         public int[] Vel { get; } = new int[3];
 
-         public int VX { get; set; } = 0;
-         public int VY { get; set; } = 0;
-         public int VZ { get; set; } = 0;
-
-         public int KineticEnergy => Math.Abs(VX) + Math.Abs(VY) + Math.Abs(VZ);
-         public int PotentialEnergy => Math.Abs(X) + Math.Abs(Y) + Math.Abs(Z);
+         public int KineticEnergy => Vel.Sum(v => Math.Abs(v));
+         public int PotentialEnergy => Pos.Sum(p => Math.Abs(p));
          public int TotalEnergy => PotentialEnergy * KineticEnergy;
       }
    }
