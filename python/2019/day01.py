6a72a8e69b74604fdf906ba5bdbffcd16c15284e
nickspoon <nick@jense.nz>
2020-11-21 22:58:44 +1300

    Day 1, 2019: python

---
+++ python/2019/day01.py
@@ -0,0 +1,13 @@
+# Open the input file
+with open("day01-input") as f:
+    # Read the inputs line by line into variable "inputs"
+    inputs = list(f)
+
+# "inputs" now contains a list of "strings", one for each line of the input.
+# Convert these to integers, so they can be manipulated mathematically
+input_ints = [int(line) for line in inputs]
+
+# Do some maths on each line, and sum all the results into variable "fuel"
+fuel = sum(line // 3 - 2 for line in input_ints)
+
+print(fuel)
