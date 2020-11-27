e23bc5b755817d8634ed479d2662d68a1b3aaace
nickspoon <nick@jense.nz>
2020-11-27 23:25:04 +1300

    Add runner script to run problems by year/day/part

---
+++ python/aoc.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+"""
+Run python advent of code programs.
+
+Usage:  ./aoc.py <year> <day> [b]
+
+  eg.   ./aoc.py 2019 1
+        ./aoc.py 2019 1 b
+"""
+
+import importlib
+import sys
+
+
+def run(year, day, a_or_b):
+    """Load and run the requested program"""
+    filename = f"{year}.aoc_{day:02d}_{'b' if a_or_b else 'a'}"
+
+    importlib.import_module(filename, f"{year}")
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 3:
+        print(__doc__)
+        sys.exit(0)
+
+    if not sys.argv[1].isnumeric() or not sys.argv[2].isnumeric():
+        sys.exit("Invalid argument type")
+
+    arg_year = int(sys.argv[1])
+    arg_day = int(sys.argv[2])
+    arg_a = len(sys.argv) > 3
+
+    run(arg_year, arg_day, arg_a)
