ad593a536b722069f43ee8e23ff837b31aff5352
nickspoon <nick@jense.nz>
2020-12-01 23:46:25 +1300

    Add template

---
+++ python/aoc_template.py
@@ -0,0 +1,15 @@
+""" Advent of Code, 2020: Day 00, [ab] """
+
+with open(__file__[:-5] + "_input") as f:
+    inputs = list(f)
+input_ints = [int(line) for line in inputs]
+
+
+def run():
+    # ...
+
+    return 0
+
+
+if __name__ == "__main__":
+    print(run())
