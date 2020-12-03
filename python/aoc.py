cb3bd9ed022513e5bf55f02bb42860270f546232
nickspoon <nick@jense.nz>
2020-12-03 23:58:01 +1300

    Add input download functionality

--- python/aoc.py
+++ python/aoc.py
@@ -7,16 +7,24 @@ Usage:  ./aoc.py <year> <day> [b]
 
   eg.   ./aoc.py 2019 1
         ./aoc.py 2019 1 b
+
+Download the day's input file:
+        ./aoc.py 2020 5 input
+
+Note: downloading the input file requires a valid session cookie, which can be
+fetched from browser developer tools.
 """
 
 import importlib
 import sys
 import time
 
+import requests
+
 
 def run(year, day, a_or_b):
-    """Load and run the requested program"""
-    filename = f"{year}.aoc_{day:02d}_{'b' if a_or_b else 'a'}"
+    """ Load and run the requested program """
+    filename = f"{year}.aoc_{day:02d}_{a_or_b}"
 
     mod = importlib.import_module(filename, f"{year}")
     start = time.time()
@@ -24,6 +32,25 @@ def run(year, day, a_or_b):
     end = time.time()
 
     print(f"{result:<20} in {end - start:0.5f}s")
+
+
+def download_input(year, day):
+    """ Request the day's input file from https://adventofcode.com """
+    with open("session_cookie") as f:
+        session_cookie = f.readline().strip()
+
+    try:
+        url = f"https://adventofcode.com/{year}/day/{day}/input"
+        cookies = {"session": session_cookie}
+        req = requests.get(url, cookies=cookies)
+        req.raise_for_status()
+    except requests.exceptions.RequestException as err:
+        if err.response.status_code == 400:
+            print(err.response.text.strip())
+
+    filename = f"{year}/aoc_{day:02d}_input"
+    with open(filename, "w") as f:
+        f.write(req.text)
 
 
 if __name__ == "__main__":
@@ -36,6 +63,13 @@ if __name__ == "__main__":
 
     arg_year = int(sys.argv[1])
     arg_day = int(sys.argv[2])
-    arg_a = len(sys.argv) > 3
+    arg_part = ""
+    if len(sys.argv) > 3:
+        arg_part = sys.argv[3]
 
-    run(arg_year, arg_day, arg_a)
+    if arg_part == "input":
+        download_input(arg_year, arg_day)
+    elif arg_part in ["a", "b"]:
+        run(arg_year, arg_day, arg_part)
+    else:
+        print(f"Invalid part: {arg_part}")
