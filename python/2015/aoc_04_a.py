f55a2a90874ab4859198f461f5ffdb600d0c338a
nickspoon <nick@jense.nz>
2020-12-04 23:42:44 +1300

    Day 4, 2015

---
+++ python/2015/aoc_04_a.py
@@ -0,0 +1,23 @@
+""" Advent of Code, 2015: Day 04, a """
+
+import hashlib
+
+with open(__file__[:-5] + "_input") as f:
+    inputs = list(f)
+word = inputs[0].strip()
+
+
+def run():
+    """ Brute force: generating md5 hashes of strings """
+    n = 0
+    while True:
+        h = hashlib.md5((word + str(n)).encode("utf-8")).hexdigest()
+        if h[:5] == "00000":
+            return n
+        n += 1
+
+    return 0
+
+
+if __name__ == "__main__":
+    print(run())
