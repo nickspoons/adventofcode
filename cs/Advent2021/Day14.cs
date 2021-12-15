d4b8b1ec09cc9aea427551e2f4ec2a42dafae230
nickspoon <nick@jense.nz>
2021-12-15 13:46:20 +1300

    Day 14 b, 2021

--- cs/Advent2021/Day14.cs
+++ cs/Advent2021/Day14.cs
@@ -1,40 +1,77 @@
 using System.Collections.Generic;
 using System.Linq;
 
+#pragma warning disable IDE0008
+
 namespace AdventOfCode.Advent2021 {
    public class Day14 : AdventDay {
       public override int Day => 14;
       public override int Year => 2021;
 
-      private Dictionary<string, char> Rules;
-      private string Template;
+      private Dictionary<string, Rule> Rules;
+      private Dictionary<char, long> Frequencies;
+      private Dictionary<string, Dictionary<char, long>> FreqCache;
+      private List<string> Template;
 
       private void ParseInput() {
-         Rules = new Dictionary<string, char>();
-         Template = InputLines[0];
+         Dictionary<string, string> inRules = new Dictionary<string, string>();
+         Frequencies = new Dictionary<char, long>();
+         FreqCache = new Dictionary<string, Dictionary<char, long>>();
+         Template = new List<string>();
+         string template = InputLines[0];
+         for (int i = 0; i < template.Length - 1; i++)
+            Template.Add(template.Substring(i, 2));
          foreach (string line in InputLines.Skip(1)) {
             string[] parts = line.Split(" -> ");
-            Rules.Add(parts[0], parts[1][0]);
+            inRules.Add(parts[0], $"{parts[0][0]}{parts[1]}");
+            char key = parts[1][0];
+            if (!Frequencies.ContainsKey(key))
+               Frequencies.Add(key, 0);
+         }
+         Rules = new Dictionary<string, Rule>();
+         foreach (string key in inRules.Keys) {
+            string a = inRules[key];
+            string b = $"{a[1]}{key[1]}";
+            char c = a[1];
+            Rules.Add(key, new(a, b, c));
+         }
+         foreach (char c in template)
+            Frequencies[c]++;
+      }
+
+      private void AddFrequencies(Dictionary<char, long> cached) {
+         foreach (char key in cached.Keys)
+            Frequencies[key] += cached[key];
+      }
+
+      private void ApplyRule(string key, int times) {
+         if (times == 0)
+            return;
+         string cacheKey = $"{key}:{times}";
+         if (FreqCache.ContainsKey(cacheKey))
+            AddFrequencies(FreqCache[cacheKey]);
+         else {
+            var snapshot = Frequencies
+               .ToDictionary(f => f.Key, f => f.Value);
+            Rule r = Rules[key];
+            Frequencies[r.c]++;
+            ApplyRule(r.a, times - 1);
+            ApplyRule(r.b, times - 1);
+            FreqCache.Add(cacheKey, Frequencies
+               .ToDictionary(f => f.Key, f => f.Value - snapshot[f.Key]));
          }
       }
 
       private void ApplyRules(int times) {
-         int n = 0;
-         while (n++ < times) {
-            string next = Template.Substring(0, 1);
-            for (int i = 0; i < Template.Length - 1; i++) {
-               char c = Rules[Template.Substring(i, 2)];
-               next = $"{next}{c}{Template[i + 1]}";
-            }
-            Template = next;
-         }
+         foreach (string element in Template)
+            ApplyRule(element, times);
       }
 
-      private int CommonalityDifference() {
-         IEnumerable<IGrouping<char, char>> grouped = Template.ToCharArray().GroupBy(c => c);
-         int most = grouped.Max(g => g.Count());
-         int fewest = grouped.Min(g => g.Count());
-         return most - fewest;
+      private long CommonalityDifference() {
+         var ordered = Frequencies.OrderBy(freq => freq.Value);
+         var gfewest = ordered.First();
+         var gmost = ordered.Last();
+         return gmost.Value - gfewest.Value;
       }
 
       public override string A() {
@@ -45,10 +82,10 @@ namespace AdventOfCode.Advent2021 {
 
       public override string B() {
          ParseInput();
-         // ApplyRules(40);
-         // 17 takes 20 seconds ... 40 is out of the question
-         ApplyRules(17);
+         ApplyRules(40);
          return CommonalityDifference().ToString();
       }
+
+      private record Rule(string a, string b, char c);
    }
 }
